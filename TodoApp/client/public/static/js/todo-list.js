dea62f6c0704d964403afb8ca21a0dd7676c5c11
Jose <sixgg4@gmail.com>
2018-10-11 21:44:48 -0400

    Added get route

--- TodoApp/client/public/static/js/todo-list.js
+++ TodoApp/client/public/static/js/todo-list.js
@@ -1,48 +1,75 @@
 /* When document ready */
 $(document).ready(() => {
 
-  /*  When clicking submit */
-  $('form').on('submit', () => {
-    /*     create a variable name item */
-    let item = $('form input');
-    
-    let todo = {
-      item: item.val()
-    };
-    console.log(todo)
-    /* We manipulate the data then 
-      Make ajax request to server goes to controller
-    */
+  
+    /* Making a "Get" call to the api */
+    let todos;
     $.ajax({
-      type: 'POST',
-      // url: '/todo',
-      data: todo,
-      success: (data) => {
-        // location.reload();
-        console.log('Success!')
-      }
+        type: 'get',
+        url: '/api/todo',
+        success: (data) => {
+            todos = data
+            for (let tasks of todos) {
+                /*console.log(`This is from the for loop: ${tasks.item}`)*/
+                $('#render').append(`<li>${tasks.item}</li>`);
+            }
+        },
+        error: function (err, data) {
+            console.log(data)
+            console.log(err)
+            console.log("Error " + err.responseText);
+        },
+        return: false
+
+    })
+
+
+    /*  When clicking submit */
+    $('form').on('submit', (e) => {
+        e.preventDefault(e)
+        /*     create a variable name item */
+        let item = $('form input')
+
+        let todo = {
+            item: item.val()
+        };
+        //console.log(todo)
+        /* We manipulate the data then 
+          Make ajax request to server goes to controller
+        */
+        $.ajax({
+            type: 'POST',
+            url: '/todo',
+            data: todo,
+            success: (data) => {
+                //location.reload();
+                console.log(`This is from the data: ${data}`)
+                console.log(JSON.stringify(data))
+            },
+            error: function (err, data) {
+                console.log("Error " + err.responseText);
+            },
+            return: false
+
+        })
+
     });
 
-    return false;
-
-  });
-
-  $('li').on('click', function() {
-    /* Replace spaces with hyphens */
-    // console.log('hit click')
-    let item = $(this).text().trim().replace(/ /g, "-")
-    console.log('this is from click ' + item)
-    $.ajax({
-      type: 'DELETE',
-      url: '/todo/' + item,
-      success: function(data) {
-        //reload page
-        location.reload();
-      },
-      error: function (err, data) {
-        alert("Error " + err.responseText);
-      }
+    $('li').on('click', function () {
+        /* Replace spaces with hyphens */
+        let item = $(this).text().trim().replace(/ /g, "-")
+        /* console.log('this is from click ' + item) */
+        $.ajax({
+            type: 'DELETE',
+            url: '/todo/' + item,
+            success: function (data) {
+                //reload page
+                location.reload();
+            },
+            error: function (err, data) {
+                alert("Error " + err.responseText);
+            }
+        });
     });
-  });
 
 });
--- TodoApp/server/config/routes.js
+++ TodoApp/server/config/routes.js
@@ -1,16 +1,23 @@
 const path = require('path');
 const mongoose = require("mongoose");
-// const TodoController = require('../controllers/TodoController.js');
-//connect to database
-mongoose.connect('mongodb://test:test12345@ds117773.mlab.com:17773/todo-app', { useNewUrlParser: true })
+const TodoController = require('../controllers/TodoController.js');
+/*connect to database*/
+mongoose.connect('mongodb://test:test12345@ds117773.mlab.com:17773/todo-app', {
+    useNewUrlParser: true
+})
 
 /* Create a schema based of the import */
 let TodoSchema = new mongoose.Schema({
-	item:{type:String,required:true,minlength:2,maxlength:64}
+    item: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 64
+    }
 });
 
 /* todo is based off of the TaskSchema */
-let Todo = mongoose.model("Todo",TodoSchema);
+let Todo = mongoose.model("Todo", TodoSchema);
 
 
 /* This is exporting a function which takes in the app and handles all the requests */
@@ -19,32 +26,84 @@ module.exports = (app) => {
         res.render('index')
     })
 
-    app.get('/todo', (req, res) => {
-        Todo.find({}, (err, data) => {
-            if (err) {throw err}
-            res.render('todo', {todos:data}) 
+    /* app.get('/todo', (req, res) => {
+         Todo.find({}, (err, data) => {
+             if (err) { throw err }
+             res.render('todo', { todos: data })
+         })
+     })
+     */
+
+    //app.post('/todo', (req, res) => {
+    //    console.log('hit here')
+    //    /* todolist.js the data is being catch by ajax */
+    //    let newTodo = Todo(req.body).save(function (err, data) {
+    //        if (err) throw err
+    //        res.json(data)
+    //    })
+    //}) 
+
+    app.delete('/todo/:item', (req, res) => {
+        /* takes the data and assigns it as todo */
+        Todo.find({ item: req.params.item.replace(/\-/g, " ") }).deleteOne(function (err, data) {
+            if (err) { throw err }
+            res.json(data)
         })
     })
 
+
+
+    /* Testing Ajax Routes, due to replace the regular routes */
+
+
+    app.get('/todo', (req, res) => {
+        res.render('todo-ajax')
+    })
+
+    app.get('/api/todo', (req, res) => {
+        Todo.find({}, (err, data) => {
+            if (err) {
+                res.json(err)
+            } else {
+                res.json(data)
+            }
+
+        })
+    })
+
+
+
     app.post('/todo', (req, res) => {
-        console.log('hit here')
         /* todolist.js the data is being catch by ajax */
-        let newTodo = Todo(req.body).save(function(err, data){
-            if (err) throw err
-            res.json(data)
+        let newTodo = Todo(req.body).save( (err, data) => {
+            console.log('====================================');
+            console.log(`This is from the data: ${data}`);
+            console.log('====================================');
+            console.log('====================================');
+            console.log(`This is from the err: ${err}`);
+            console.log('====================================');
+            if (err) {
+                res.json(err)
+            } res.json(data)
+            
         })
     })
 
     app.delete('/todo/:item', (req, res) => {
         /* takes the data and assigns it as todo */
-        Todo.find({item: req.params.item.replace(/\-/g, " ")}).deleteOne(function(err, data){
-            if (err) {throw err}
-            res.json(data)
-            })
+        Todo.find({
+            item: req.params.item.replace(/\-/g, " ")
+        }).deleteOne(function (err, data) {
+            if (err) {
+                res.json(err)
+            } else {
+                res.json(data)
+            }
         })
-
+    })
 
     app.all('*', (req, res) => {
         res.sendFile(path.resolve('./client/public/views/404.html'));
     })
+
 };
